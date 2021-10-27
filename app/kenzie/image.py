8e6e70cf22a6bbd950724599c332a46df876b027
Nicole Pimenta <niipimenta@hotmail.com>
2021-10-27 19:27:08 -0300

    feat:rota get /list

--- app/kenzie/image.py
+++ app/kenzie/image.py
@@ -1,14 +1,39 @@
 import os
-import dotenv 
-from flask.helpers import safe_join
+
+import dotenv
 
 dotenv.load_dotenv() 
 
+from flask.helpers import safe_join
+from werkzeug.datastructures import FileStorage
+from datetime import datetime , timezone
+from werkzeug.utils import secure_filename
+
+
 FILES_DIRECTORY = os.environ.get("FILES_DIRECTORY") 
 
-def list_all_directories():
-    all_directories = os.listdir(FILES_DIRECTORY) 
-    return list(all_directories) 
+
+def list_all_files():
+    all_files = os.listdir(FILES_DIRECTORY) 
+    return list(all_files) 
+
+def get_path(filename:str):
+    path = safe_join(FILES_DIRECTORY,filename)
+    return path  
+
+def list_files(path):
+    files = os.listdir(path)
+    return files
+
+def get_extension(filename:str):
+    extension = filename.split(".")[-1] 
+    return extension
+
+
+def list_all_images_files():
+    files_list = os.listdir(FILES_DIRECTORY)
+    return files_list
+
 
 
 def check_if_empty_repo_exist():
@@ -20,11 +45,30 @@ def check_if_empty_repo_exist():
 
     return all(verify)   
 
-def list_all_images():
-    _,_,images_list =list(os.walk(FILES_DIRECTORY))[0]
-    return images_list
 
+
+
+
+
+
+
+def png_path(filename:str):
+    path = safe_join(f'{FILES_DIRECTORY}/png',filename)
+    return path 
+
+def save_image(file: FileStorage):
+    file_extension = file.filename.split(".")[-1]
+    filename = str(datetime.now(timezone.utc))[:26] 
+    filename = secure_filename(filename) 
+    filename = f'{filename}.{file_extension}'
+    path = safe_join(FILES_DIRECTORY, filename)
     
-   
-def get_path(filename:str):
-    path = safe_join(FILES_DIRECTORY,filename)
+    file.save(path)
+    return filename
+
+
+def dowload_zip_dir(path):
+    comando = f'{path} touch nicole.txt'
+    os.system(comando)
+    
+    
